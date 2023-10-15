4a4af087a37ffb7d6e5b86a5e459c3ebd7840740
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2023-10-15 11:09:51 +0500

    add solution number 1

---
+++ lib/test1.py
@@ -0,0 +1,22 @@
+try:
+    fileName = "text_1_var_12"
+    allWords = dict()
+    
+    with open("assets/data/1/"+fileName, "r") as file:
+        allLines = file.readlines()
+        for line in allLines:
+            row = line.strip().replace(","," ").replace("."," ").replace("?"," ").replace("!"," ").strip()
+            allWordsInARow = row.split(" ")
+            for wordInARow in allWordsInARow:
+                if(wordInARow in allWords):
+                    allWords[wordInARow] += 1
+                else:
+                    allWords[wordInARow] = 1
+        allWords = dict(sorted(allWords.items(), reverse=True))
+        
+    with open("assets/result/1/"+fileName+"_result", "w") as result:
+        for key, value in allWords.items():
+            result.write(key + " : " + str(value) + "\n")
+        
+except Exception as e:
+    print(e)
