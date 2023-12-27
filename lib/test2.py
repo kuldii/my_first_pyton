bff9f13197222138fa515fbea8903d23b6e92075
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2023-12-27 14:27:38 +0500

    fix number 2

--- lib/test2.py
+++ lib/test2.py
@@ -1,19 +1,15 @@
-try:
-    fileName = "text_2_var_12"
-    allSumNumbers = []
+fileName = "text_2_var_12"
+allSumNumbers = []
+
+with open("assets/data/2/"+fileName, "r") as file:
+    allLines = file.readlines()
+    for line in allLines:
+        sum = 0
+        allNumbersInARow = line.strip().split(";")
+        for numberInARow in allNumbersInARow:
+            sum += int(numberInARow)
+        allSumNumbers.append(sum)
     
-    with open("assets/data/2/"+fileName, "r") as file:
-        allLines = file.readlines()
-        for line in allLines:
-            allNumbersInARow = line.strip().split(";")
-            sum = 0
-            for numberInARow in allNumbersInARow:
-                sum += int(numberInARow)
-            allSumNumbers.append(sum)
-        
-    with open("assets/result/2/"+fileName+"_result", "w") as result:
-        for value in allSumNumbers:
-            result.write(str(value) + "\n")
-        
-except Exception as e:
-    print(e)
+with open("assets/result/2/"+fileName+"_result", "w") as result:
+    for value in allSumNumbers:
+        result.write(str(value) + "\n")
