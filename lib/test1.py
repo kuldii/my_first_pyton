a6628b8e1a54a20775c852a98489839a3b7881e9
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2023-12-27 14:23:44 +0500

    fix number 1

--- lib/test1.py
+++ lib/test1.py
@@ -1,22 +1,17 @@
-try:
-    fileName = "text_1_var_12"
-    allWords = dict()
+fileName = "text_1_var_12"
+allWords = dict()
+
+with open("assets/data/1/"+fileName, "r") as file:
+    allLines = file.readlines()
+    for line in allLines:
+        allWordsInARow = line.strip().replace(","," ").replace("."," ").replace("?"," ").replace("!"," ").strip().split(" ")
+        for wordInARow in allWordsInARow:
+            if(wordInARow in allWords):
+                allWords[wordInARow] += 1
+            else:
+                allWords[wordInARow] = 1
+    allWords = dict(sorted(allWords.items(), reverse=True))
     
-    with open("assets/data/1/"+fileName, "r") as file:
-        allLines = file.readlines()
-        for line in allLines:
-            row = line.strip().replace(","," ").replace("."," ").replace("?"," ").replace("!"," ").strip()
-            allWordsInARow = row.split(" ")
-            for wordInARow in allWordsInARow:
-                if(wordInARow in allWords):
-                    allWords[wordInARow] += 1
-                else:
-                    allWords[wordInARow] = 1
-        allWords = dict(sorted(allWords.items(), reverse=True))
-        
-    with open("assets/result/1/"+fileName+"_result", "w") as result:
-        for key, value in allWords.items():
-            result.write(key + " : " + str(value) + "\n")
-        
-except Exception as e:
-    print(e)
+with open("assets/result/1/"+fileName+"_result", "w") as result:
+    for key, value in allWords.items():
+        result.write(key + " : " + str(value) + "\n")
